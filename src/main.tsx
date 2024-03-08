878e7bca61f8c06464be87b64f619509ba3bc4d9
Dzumanet <137023861+Dzumanet@users.noreply.github.com>
2024-03-08 07:55:23 +0100

    Create interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+interface Experience {
+  year: number;
+  description: string;
+}
+
+interface CVData {
+  photo: string;
+  name: string;
+  lastName: string;
+  experience: Experience[];
+  education: string[];
+}
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
    
