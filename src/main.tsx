67d588f5db9f56dee0ac2aaf610dfade5605cb50
Dzumanet <137023861+Dzumanet@users.noreply.github.com>
2024-03-08 08:03:40 +0100

    Create CvData and html structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CvPhoto from '../src/assets/cvAlien.webp'
+
+
 
 import './index.css'
 
@@ -15,11 +18,36 @@ interface CVData {
   experience: Experience[];
   education: string[];
 }
+ const CvData: CVData = {
+  photo: CvPhoto,
+  name: 'Konrad',
+  lastName: 'Alien',
+  experience: [
+    { year: 2020, description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, quasi" },
+    { year: 2021, description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, quasi" },
+    { year: 2023, description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, quasi" }
+  ],
+  education: [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    "Placeat consequuntur",
+    "Placeat consequuntur, impedit sapiente incidunt reprehenderit doloribus laboriosam.",
+    "Placeat, autem enim? Ratione illum, facere porro adipisci alias aliquam tenetur iste reiciendis nobis",
+  ]
 
+
+ }
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   
+   <main className="cv-main">
+    <aside className="cv-aside">
+
+    </aside>
+    <section className="cv-section">
+      <section className="cv-section-experience"></section>
+      <section className="cv-section-education"></section>
+    </section>
+   </main>
   </React.StrictMode>,
 )
